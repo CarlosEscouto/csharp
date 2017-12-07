6eda22408b31177628c5b1d127a1116b046b1a82
carlosvendedoor <carlos@vendedoor.com.br>
2017-12-07 19:25:25 -0200

    Atualização para o web

--- Modelo/Model/DAO/Configuracao/Configuracoes.cs
+++ Modelo/Model/DAO/Configuracao/Configuracoes.cs
@@ -16,6 +16,7 @@ namespace Model.DAO.Configuracao
                 //strConexao = "Data Source = (LocalDB)\\MSSQLLocalDB; Initial Catalog = smartdb; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
                 /*String online*/
                 strConexao = "Server=tcp:integriert.database.windows.net,1433;Initial Catalog=smartdb;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                                                    
             }
             // Pra que? \/ nunca vai cair nessa condição
             catch(Exception ex)
